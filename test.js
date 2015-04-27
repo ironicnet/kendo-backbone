019a8d07632a9b91c8df1508b05e6c9d40a2ffbf
Travis Webb <me@traviswebb.com>
2015-04-27 01:58:21 -0400

    setup initial test harness

--- test.js
+++ test.js
@@ -1,6 +1,40 @@
 var _ = require('lodash');
+var cp = require('cp-r');
+var rimraf = require('rimraf');
 var assert = require('assert');
+var path = require('path');
 var jsdom = require('jsdom');
+var SailsApp = require('sails').Sails;
+var BackboneClient;
+
+var sailsConfig = {
+  appPath: path.dirname(require.resolve('hashware-api')),
+  hooks: {
+    grunt: false
+  },
+  config: {
+    permissions: {
+      adminUsername: 'admin@sailsjs.org',
+      adminPassword: 'admin1234',
+      adminEmail: 'admin@sailsjs.org'
+    }
+  },
+  models: {
+    connection: 'unittest-memory'
+  },
+  connections: {
+    'unittest-memory': {
+      adapter: 'sails-memory'
+    }
+  },
+  log: {
+    level: 'error'
+  },
+  port: 1444
+};
+var url = 'http://localhost:1444/api/v1/backbonemodel';
+var app = new SailsApp();
+var sdk = { };
 
 describe('kendo-backbone', function () {
   var env;
@@ -10,6 +44,11 @@ describe('kendo-backbone', function () {
       assert.throws(function () {
         require('./');
       }, Error);
+    });
+    it('copy memory adapter into appPath due to sails issue', function (done) {
+      rimraf.sync('node_modules/hashware-api/node_modules/sails-memory');
+      cp('node_modules/sails-memory', 'node_modules/hashware-api/node_modules/sails-memory')
+        .read(done);
     });
   });
 
@@ -25,11 +64,60 @@ describe('kendo-backbone', function () {
         ],
         done: function (err, window) {
           global.kendo = window.kendo;
+          global._window = window;
           done(err);
         }
       });
     });
 
+    it('should load kendo.data.Backbone', function () {
+      var kdb = kendo.data.Backbone = require('./');
+      assert(kdb.Model);
+      assert(kdb.Collection);
+      assert(kdb.DataSource);
+    });
+
+  });
+
+  describe('kendo.data.Backbone.DataSource', function () {
+    var kendoDataSource, backboneDataSource;
+
+    before(function (done) {
+      this.timeout(60 * 1000);
+
+      global.$ = {
+        ajax: require('najax')
+      };
+      BackboneClient = require('sails-backbone-client');
+
+      app.lift(sailsConfig, function (error, sails) {
+        if (error) return done(error);
+
+        BackboneClient.create(url, { })
+          .then(function (_sdk) {
+            sdk = _sdk;
+            done(error);
+          })
+          .catch(done);
+      });
+    });
+
+    it('sanity check sails-permissions models', function () {
+      assert(sdk.Permission);
+      assert(sdk.Model);
+      assert(sdk.Role);
+      assert(sdk.User);
+    });
+    
+    it('can define new vanilla kendo.data.DataSource without error', function () {
+      kendoDataSource = new kendo.data.DataSource({
+        url: 'http://localhost:1444/permission'
+      });
+    });
+
+    it('can define new Backbone.DataSource without error', function () {
+
+    });
 
   });
 });
